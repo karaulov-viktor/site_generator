f3abd76c64a379350ed9968072534605f4429c7c
Karylov <karaylov-viktor@mail.ru>
2022-02-17 19:57:29 +0300

    123

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -1,4 +1,4 @@
-"""passwor_generator URL Configuration
+"""password_generator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
